ed7ea91e31a0ecf3b71bbf84d7bc64519da4bdd8
Shubham Rajpoot <120890455+Shubham-Rajpoot@users.noreply.github.com>
2024-11-15 14:14:26 +0530

    latest job made

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './shared/Navbar'
 import HeroSection from './HeroSection'
+import LatestJobs from './LatestJobs'
 
 const Home = () => {
   return (
     <div>
       <Navbar/>
       <HeroSection />
+      <LatestJobs/>
     </div>
   )
 }
