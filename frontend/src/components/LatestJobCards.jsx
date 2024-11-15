0590dcf47648b3e0895ed803c97d02ff78f3df3b
Shubham Rajpoot <120890455+Shubham-Rajpoot@users.noreply.github.com>
2024-11-15 22:25:35 +0530

    browse and navbar-section updated

--- frontend/src/components/LatestJobCards.jsx
+++ frontend/src/components/LatestJobCards.jsx
@@ -3,7 +3,7 @@ import { Badge } from './ui/badge'
 
 const LatestJobCards = () => {
   return (
-    <div className='p-5 rounded-md shadow-2xl shadow-purple-200 bg-white border border-gray-300 cursor-pointer'>
+    <div className='p-5 rounded-md shadow-2xl shadow-purple-300 bg-white border border-gray-300 cursor-pointer'>
         <div>
         <h1 className='font-medium text-lg'>Company Name</h1>
         <p className='text-sm text-gray-500'>India</p>
--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -9,9 +9,10 @@ import { toast } from "sonner";
 import axios from 'axios';
 import { USER_API_END_POINT } from '@/utils/constant'; 
 import { useDispatch, useSelector } from "react-redux";
-import { setLoading } from "@/redux/authSlice";
+import { setLoading, setUser } from "@/redux/authSlice";
 import store from "@/redux/store";
 import { Loader2 } from "lucide-react";
+
 
 
 const Login = () => {
@@ -40,6 +41,7 @@ const Login = () => {
         })
         if(res.data.success)
         {
+          dispatch(setUser(res.data.user));
           navigate("/");
           toast.success(res.data.message);
         }
